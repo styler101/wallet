a608d24afd91546b7c4f4daa20cae466b802f5dd
Lucas <lucas.santos@mundowap.com.br>
2023-02-06 21:31:30 -0300

    refactor: header and signin tests

--- src/components/Header/tests/header.spec.tsx
+++ src/components/Header/tests/header.spec.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import { Header } from '../ui'
-import { render, screen } from '@testing-library/react'
+import { render, type RenderResult } from '@testing-library/react'
+
+type SutTypes = {
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  return {
+    sut: render(<Header hasAnimation={true} iconSize={10} />)
+  }
+}
 
 describe('Header Component', () => {
   test('ensure header component receive a correct size property', () => {
@@ -12,11 +22,8 @@ describe('Header Component', () => {
   })
 
   test('ensure header component has a correct heading title', () => {
-    const fakeProps = {
-      iconSize: 18
-    }
-    render(<Header iconSize={fakeProps.iconSize} hasAnimation={false} />)
-    const heading = screen.getByRole('heading', { level: 4 })
+    const { sut } = makeSut()
+    const heading = sut.getByRole('heading', { level: 4 })
     expect(heading.innerHTML).toEqual(' Minha Carteira')
   })
 })
--- src/pages/Signin/tests/signin.spec.tsx
+++ src/pages/Signin/tests/signin.spec.tsx
@@ -1,19 +1,29 @@
 import React, { type HTMLAttributes } from 'react'
 import { SignIn } from '../ui'
-import { render, screen } from '@testing-library/react'
+import { render, type RenderResult } from '@testing-library/react'
+
+type SutTypes = {
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  return {
+    sut: render(<SignIn />)
+  }
+}
 
 describe('Signin Component', () => {
   test('Should start signin state with default values', () => {
-    render(<SignIn />)
-    const button = screen.getByRole('button', {
+    const { sut } = makeSut()
+    const button = sut.getByRole('button', {
       name: /acessar/i
     }) as HTMLButtonElement
     expect(button.disabled).toBe(true)
-    const email = screen.getByPlaceholderText(
+    const email = sut.getByPlaceholderText(
       /informe seu email/i
     ) as HTMLInputElement
     expect(email.value).toBe('')
-    const password = screen.getByPlaceholderText(
+    const password = sut.getByPlaceholderText(
       /informe sua senha/i
     ) as HTMLInputElement
     expect(password.value).toBe('')

a7b781660fd61071f3c1f5e96b8f3f408aeeb392
Lucas <lucas.santos@mundowap.com.br>
2023-02-07 20:20:07 -0300

    chore: add generatetoken method

--- src/utitls/validators/index.ts
+++ src/utitls/validators/index.ts
@@ -1,3 +1,8 @@
+import { v4 } from 'uuid'
 export function isValidEmail(email: string): boolean {
   return /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/.test(email)
+}
+
+export function generateId(): string {
+  return v4()
 }

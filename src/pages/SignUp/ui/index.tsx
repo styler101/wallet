c8b6a7089d78af4802d70eebb57f0daa3646fe40
Lucas <lucas.santos@mundowap.com.br>
2023-02-10 06:51:18 -0300

    feat: initial signup page

---
+++ src/pages/SignUp/ui/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as S from './styles'
+
+export function SignUp() {
+  return (
+    <S.Container>
+      <h1> SignUp Page</h1>
+    </S.Container>
+  )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes as Switch, Route } from 'react-router-dom'
 import { SignIn } from '@/pages/Signin/ui'
+import { SignUp } from '@/pages/SignUp/ui'
 import { Error404 } from '@/pages/Error404/ui'
 
 export function Routes() {
@@ -8,6 +9,7 @@ export function Routes() {
     <Switch>
       <Route path="*" element={<Error404 />} />
       <Route element={<SignIn />} index />
+      <Route path="/signup" element={<SignUp />} />
     </Switch>
   )
 }
